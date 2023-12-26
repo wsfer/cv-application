50adc5e2c8488b7ca273af7b68b4e1d6fd7626c0
wsfer <93054415+wsfer@users.noreply.github.com>
2023-12-26 16:41:27 -0300

    feat: add form and resume on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Resume from './components/Resume';
+import FormController from './components/FormController';
+import './App.css';
+
+const placeholderResume = {
+  name: 'your name',
+  contacts: {
+    github: 'github.com/you',
+    linkedin: 'linkedin.com/asd',
+    email: 'email@email.com',
+    phone: '',
+    website: '',
+  },
+  summary:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem earum obcaecati dicta quisquam corporis suscipit voluptatum hic quod consequatur alias enim nam, consectetur totam repellendus impedit. Necessitatibus ea magnam ad?',
+  experiences: [
+    {
+      id: crypto.randomUUID(),
+      title: 'job title',
+      company: 'company',
+      start: new Date(),
+      end: null,
+      activities: ['activity 1', 'activity 2'],
+    },
+    {
+      id: crypto.randomUUID(),
+      title: 'job 2',
+      company: 'company name',
+      start: new Date(),
+      end: new Date(),
+      activities: ['activity'],
+    },
+  ],
+  projects: [
+    {
+      id: crypto.randomUUID(),
+      name: 'project',
+      link: 'github.com/your/project',
+      activities: ['activity 1', 'activity 2'],
+    },
+    {
+      id: crypto.randomUUID(),
+      name: 'project name',
+      link: 'github.com/your/project-name',
+      activities: ['activity 1', 'activity'],
+    },
+  ],
+  skills: {
+    tools: 'figma, mathlab',
+    languages: 'javascript, C++, python',
+    frameworks: 'vue, angular, ruby on rails',
+  },
+  education: [
+    {
+      id: crypto.randomUUID(),
+      name: 'course name',
+      location: 'university name',
+      start: new Date(),
+      end: new Date(),
+    },
+    {
+      id: crypto.randomUUID(),
+      name: 'course',
+      location: 'nothing',
+      start: new Date(),
+      end: null,
+    },
+  ],
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [resume, setResume] = useState(placeholderResume);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="main">
+      <FormController resume={resume} setResume={setResume} />
+      <Resume {...resume} />
+    </main>
+  );
 }
 
-export default App
+export default App;
