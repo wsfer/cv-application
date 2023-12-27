cbf8769af366b36d72ed186b282bb436f67a140f
wsfer <93054415+wsfer@users.noreply.github.com>
2023-12-27 15:27:56 -0300

    feat: create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../styles/Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Resume Generator</h1>
+    </header>
+  );
+}
+
+export default Header;
